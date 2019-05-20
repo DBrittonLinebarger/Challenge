b373103a185438e9c44975823ecaec7e400d597e
Denelle Britton Linebarger <brittod80@yahoo.com>
2019-05-20 16:40:59 -0600

    Sum of 1 to 100

---
+++ README.md
@@ -0,0 +1,10 @@
+# Challenge
+
+## Description
+
+This is a simple program that initially computes the sum of integers
+
+## Team members
+
+* Denelle Linebarger
+*Ahmad Musleh
--- src/edu/cnm/deepdive/AddingNumbers.java
+++ src/edu/cnm/deepdive/AddingNumbers.java
@@ -4,8 +4,8 @@ public class AddingNumbers {
 
   public static void main(String[] args) {
     int sum = 0;
-    for (int i=1; i<=100; i++)  {
-      sum = sum + i;
+    for (int i = 1; i <= 100; i++)  {
+      sum = sum + i; //sum += i;
     }
     System.out.println("The sum of 1 to 100 is " +  sum);
   }
